f94b61f0e33389d25f14da15a45519ce8b9f2ff9
Saad <saadarain1400@gmail.com>
2026-08-01 14:48:37 +0500

    Fix Staff, Student, Rental filename casing

---
+++ server/models/Student.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const studentSchema = new mongoose.Schema({
+    studentId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    studentName: {
+        type: String,
+        required: true
+    },
+    fatherName: {
+        type: String,
+        required: true
+    },
+    studentClass: {
+        type: String,
+        required: true
+    },
+    fee: {
+        type: Number,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model("Students", studentSchema);
