83b948d61d68ac773d7022957b20cd3026b75d5b
Saad <saadarain1400@gmail.com>
2026-08-07 17:15:15 +0500

    update

--- server/routes/prayerRoutes.js
+++ server/routes/prayerRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const PrayerTime = require("../models/PrayerTime");
+const PrayerTime = require("../models/Prayers");
 const protect = require("../middleware/auth");
 
 const router = express.Router();
